078430867edf8a6c96e4efb4e24c65a0743d4e05
Janek Brandt and Mark Fisher <mark@>
2017-03-23 13:47:12 -0700

    add heroes to squad assembly form and display

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,18 +8,22 @@ public class App {
   public static void main(String[] args) {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
+    // Initialize some heroes
+    Hero hero1 = new Hero("Storm", 123, "Weather control", 14, 10);
+    Hero hero2 = new Hero("Professor X", 123, "Mind control", 14, 2);
+    // Hero hero1 = new Hero();
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       model.put("squads", Squad.all());
-      model.put("squadArray", Squad.all().keySet().toArray());
+      model.put("heroes", Hero.all());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
     get("/squads/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-
+      model.put("heroes", Hero.all());
       model.put("template", "templates/squad-form.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -28,9 +32,17 @@ public class App {
       Map<String, Object> model = new HashMap<String, Object>();
       String name = request.queryParams("name");
       String cause = request.queryParams("cause");
+      String[] selectedHeroes = request.queryParamsValues("heroes");
+      // System.out.println(request.queryParamsValues("heroes").length);
       Squad newSquad = new Squad(name, cause);
+      for (String hero : selectedHeroes) {
+        // System.out.println("hero is " + hero);
+        // System.out.println(Hero.all().size());
+        // System.out.println(Hero.getHero(hero) instanceof Hero);
+        // System.out.println(Hero.getHero(hero));
+        newSquad.addHero(Hero.getHero(hero));
+      }
       model.put("squads", Squad.all());
-      model.put("squadArray", Squad.all().keySet().toArray());
 
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
