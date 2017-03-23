c26f7cd380b78f45826008c60af35c2905529a5a
Janek Brandt and Mark Fisher <mark@>
2017-03-23 11:56:12 -0700

    add routes for home, new squad and squad list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,6 +12,27 @@ public class App {
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
+      model.put("squads", Squad.all());
+      model.put("squadArray", Squad.all().keySet().toArray());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/squads/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+
+      model.put("template", "templates/squad-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/squad", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      String cause = request.queryParams("cause");
+      Squad newSquad = new Squad(name, cause);
+      model.put("squads", Squad.all());
+      model.put("squadArray", Squad.all().keySet().toArray());
+
+      model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
